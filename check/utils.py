ec1c7acd431a8ff2841f0c348733086d7efd1c24
Maksim <toytonika@ya.ru>
2024-05-08 16:03:31 +0700

    fix: change time format

--- check/utils.py
+++ check/utils.py
@@ -4,7 +4,7 @@ from datetime import datetime, timezone
 def format_timestamp(timestamp):
     """Converts the timestamp to a date string (UTC)"""
     date = datetime.fromtimestamp(timestamp, timezone.utc)
-    formatted_date = date.strftime("%Y-%m-%d %H:%M:%S %Z")
+    formatted_date = date.strftime("%d-%m-%Y %H:%M:%S %Z")
     return formatted_date
 
 
@@ -12,5 +12,5 @@ def format_iso_date(iso_date):
     """Converts the ISO date to a date string (UTC)"""
     date = datetime.fromisoformat(iso_date)
     utcfied_date = date.replace(tzinfo=timezone.utc)
-    formatted_date = utcfied_date.strftime("%Y-%m-%d %H:%M:%S %Z")
+    formatted_date = utcfied_date.strftime("%d-%m-%Y %H:%M:%S %Z")
     return formatted_date
